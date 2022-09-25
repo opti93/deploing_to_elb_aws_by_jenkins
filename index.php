da40ecd4467a240f7809d42f735029074f4a9fee
opti93 <vitaliygovarkov@gmail.com>
2022-09-25 14:11:39 +0300

    version 2.0

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ else
 <body>
     <section class="congratulations">
         <h1>Congratulations!!!!</h1>
+        <h2>You are the best Vitalii!!!</h2>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
         <p>This environment is launched with Elastic Beanstalk PHP Platform</p>
